8b251d283661a71d2903a2fd2a00961f13434d0c
Felix <barosiofelix@gmail.com>
2022-09-29 12:58:03 +0300

    Completed Assignment

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
   const [price, setPrice] = useState(0);
-  const [color, setColor] = useState("black");
+  const [color, setColor] = useState("green");
 
   useEffect(() => {
     const id = setInterval(() => setPrice(makeRandomNumber), 1000);
@@ -12,6 +12,19 @@ function Ticker() {
     };
   }, []);
 
+  const prevPriceRef = useRef(price)
+  useEffect(() => {
+    const prevPrice = prevPriceRef.current
+
+    if (price > prevPrice) {
+      setColor("green")
+    } else if (price < prevPrice) {
+      setColor("red")
+    } else {
+      setColor("black")
+    }
+    prevPriceRef.current = price
+  }, [price])
   return (
     <div>
       <h1>TickerMaster</h1>
